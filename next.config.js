10dddf1fee2283ef1cb490d2a0b808d58f1e2242
Huy Nguyen Tuan <tuanhuydev@gmail.com>
2021-10-17 19:55:33 +0700

    Revert "feat(ui): tailwind integration, implement mock home ui"

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 module.exports = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
 }
