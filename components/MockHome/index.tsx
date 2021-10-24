8188bfa698727d10cbce1d819862b5974bc1a3b8
Huy Nguyen Tuan <huynt@Huys-MacBook-Pro.local>
2021-10-24 16:16:44 +0700

    fix(8): home page styling

--- components/MockHome/index.tsx
+++ components/MockHome/index.tsx
@@ -3,67 +3,58 @@ import DownloadIcon from '@assets/images/mock/download_icon.svg';
 import FacebookIcon from '@assets/images/mock/facebook.svg';
 import GitHubIcon from '@assets/images/mock/github.svg';
 import EmailIcon from '@assets/images/mock/email.svg';
-import Head from 'next/head';
+import AvatarImage from '@assets/images/mock/avatar.svg';
 
 const MockHome = () => {
 
     return (
-        <>
-        <Head>
-            <title>Huy Nguyen - Software Engineer</title>
-            <meta name="title" content="Huy Nguyen - Software Engineer" />
-            <meta name="description" content="Hi there, I am Huy" />
-
-            {/* Open Graph / Facebook */}
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://huynt.dev/" />
-            <meta property="og:title" content="Huy Nguyen - Software Engineer" />
-            <meta property="og:description" content="Hi there, I am Huy" />
-            <meta property="og:image" content="/preview.jpeg" />
-
-            {/* Twitter */}
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://www.huynt.dev/" />
-            <meta property="twitter:title" content="Huy Nguyen - Software Engineer" />
-            <meta property="twitter:description" content="Hi there, I am Huy" />
-            <meta property="twitter:image" content="/preview.jpeg" />
-        </Head>
-        <div className="w-screen h-screen grid relative grid-rows-1 grid-cols-12 gap-1 items-center bg-mockbg animate-fade">
-            <div className="w-max col-start-1 col-span 6 self-center">
+        <div className="min-h-screen grid relative grid-rows-1 grid-cols-12 gap-1 items-center bg-mockbg animate-fade lg:gap-x-10 overflow-hidden">
+            <div className="hidden lg:block lg:col-start-1 lg:col-span-3 xl:col-start-1 xl:col-span-6 w-max self-center">
                 <HeroImage className="hidden lg:block" />
             </div>
-            <div className="col-start-7 col-span-5 self-center">
-                <h1 className="text-5xl uppercase tracking-widest font-serif leading-snug text-mockpri">nguyen <br/>tuan <br/>huy</h1>
-                <p className="max-w-xs block text-xl leading-10 mt-8 mb-14 font-thin font-san text-mocksec">
-                    <b className="block mb-2">Are you looking for a <span className="uppercase">#Developer</span> able to solve business technical issues ?</b>
+            <div className="col-span-full px-8 self-center lg:col-start-7 lg:col-end-12 lg:px-0">
+                <div className="flex flex-col sm:flex-row sm:justify-between md:flex-col lg:items-start">
+                    <AvatarImage className="block lg:hidden py-1 " />
+                    <h1 className="text-2xl my-8 uppercase tracking-widest font-serif leading-snug text-mockpri break-all sm:my-0 sm:text-4xl md:text-5xl md:my-8 lg:w-100">nguyen<br/>tuan <br/>huy</h1>
+                </div>
+                
+                <p className="block text-left leading-10 font-thin font-san text-mocksec sm:mt-8 sm:mb-10 sm:w-8/12 md:my-8 md:text-lg md:w-8/12 lg:mt-8 lg:mb-14 lg:text-base lg:w-8/12 xl:text-xl xl:w-10/12">
+                    <span className="block mb-4 text-bold text-2xl font-medium"><span className="font-extrabold uppercase">#Developer</span> to solve problem</span>
                     I&apos;m Huy, an experienced web developer contribute for years to the industry. I&apos;m not only delivering quality code but I&apos;m also being a long-term partner to your business.<br />
-                    <span className="mt-4 block">Hopefully, we have a chance to work together</span>
+                    <span className="sm:mt-2 md:mt-3 lg:mt-4 block">Hopefully, we have a chance to work together</span>
                 </p>
-                <a className="flex w-max cursor-pointer items-center py-4 px-8 bg-mockpri text-white rounded-sm hover:bg-mockprihover transition-colors duration-300 ease-in-out" href="/CV.pdf" download><DownloadIcon className="mr-2" />Resume</a>
+                <a className="flex w-max cursor-pointer items-center py-3 px-5 bg-mockpri text-white rounded-sm hover:bg-mockprihover transition-colors duration-300 ease-in-out" href="/CV.pdf" download><DownloadIcon className="mr-2" />Resume</a>
             </div>
-            <div className="grid grid-rows-3 grid-cols-1 justify-items-center items-end absolute bottom-0 right-8">
-                <div className="w-px h-32 border border-mockpri"></div>
-                <ul>
-                    <li className="mb-4">
+
+            <div className="
+                    col-start-1 col-span-full
+                    grid grid-rows-1 grid-cols-3
+                    justify-items-center items-center
+                    col-start-3 col-span-8
+                    sm:col-start-4 sm:col-span-6 sm:gap-x-1
+                    md:gap-x-2
+                    lg:grid-rows-3 lg:grid-cols-1 lg:justify-items-center lg:items-end lg:absolute lg:bottom-0 lg:right-8 lg:col-start-11">
+                <div className="h-px w-16 bg-mockpri sm:w-28 lg:h-32 lg:w-px"></div>
+                <ul className="flex flex-row justify-center items-center content-center lg:flex-col">
+                    <li className="mr-4 lg:mr-0 lg:mb-4">
                         <a href="https://www.facebook.com/tuanhuy.dev/" target="_blank" rel="noreferrer">
                             <FacebookIcon />
                         </a>
                     </li>
-                    <li className="mb-4">
+                    <li className="mr-4 lg:mr-0 lg:mb-4">
                         <a href="https://github.com/tuanhuydev" target="_blank" rel="noreferrer">
                             <GitHubIcon />
                         </a>
                     </li>
-                    <li className="mb-4">
+                    <li className="lg:mb-4">
                         <a href="mailto:tuanhuydev@gmail.com" target="_blank" rel="noreferrer">
                             <EmailIcon />
                         </a>
                     </li>
                 </ul>
-                <div className="w-px h-32 border border border-mockpri"></div>
+                <div className="h-px w-16 bg-mockpri sm:w-28 lg:h-32 lg:w-px"></div>
             </div>
         </div>
-        </>
     );
 }
 
