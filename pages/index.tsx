1131586221aaa19bc1d6ba529b84ccc4f3ac411a
tuanhuydev <tuanhuydev@gmail.com>
2022-05-02 23:57:12 +0700

    feat(home): handle skeleton loading on page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,14 @@
 import type { NextPage } from 'next'
-import AboutMe from "components/AboutMe";
-import Navbar from "components/commons/Navbar";
-import Footer from "components/Footer";
-import Hero from "components/Hero";
-import MyWork from "components/MyWork";
-import React, { useEffect, useState } from 'react';
+import React, { Suspense } from 'react';
+import PageSkeleton from 'components/commons/Seletions/PageSkeleton';
+
+const LaziedHomePage = React.lazy(() => import('containers/HomeContainer'));
 
 const Home: NextPage = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const loadingTimeout = setTimeout(() => {
-      setLoading(false);
-    }, 1500);
-    return () => clearTimeout(loadingTimeout);
-  }, []);
   return (
-    <React.Fragment>
-      {
-        loading ? (<h1>Loading</h1>) : (
-          <div className="bg-grey0 dark:bg-black min-h-screen transition-all">
-            <Navbar />
-            <Hero />
-            <AboutMe />
-            <MyWork />
-            <Footer />
-          </div>
-        )
-      }
-    </React.Fragment>
+    <Suspense fallback={(<PageSkeleton />)}>
+      <LaziedHomePage />
+    </Suspense>
   );
 }
  
